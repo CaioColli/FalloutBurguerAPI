1e5e6fb85fe2f3132e25836fa9df5a7a1f229307
CaioColli <caiocolli22@gmail.com>
2025-08-05 14:31:55 -0300

    feat chave API

---
+++ app/Http/Middleware/ApiKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $clientkey = $request->header('X-API-KEY');
+        $serverKey = env('API_KEY');
+
+        if ($clientkey !== $serverKey) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('check.api.key')->group(function () {
+    Route::prefix('auth')->group(base_path('routes/auth.php'));
 });
 
-Route::prefix('auth')->group(base_path('routes/auth.php'));
